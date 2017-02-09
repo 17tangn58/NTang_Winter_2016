ddc7032266d503af2e93843b885c3a6ef52971fa
17tangn58 <17tangn58@millshighsch.org>
2017-02-09 09:47:10 -0800

    Finished Ticket with Tester

--- Ticket/src/StudentAdvanceTicket.java
+++ Ticket/src/StudentAdvanceTicket.java
@@ -3,10 +3,10 @@ public class StudentAdvanceTicket extends AdvanceTicket {
 	public StudentAdvanceTicket(int n, int days){
 		super(n, days);
 	}
-	public int getPrice(){
-		if(getDays()>=10)
-			return 15;
-		else
-			return 20;
+	public double getPrice(){
+		return super.getPrice()/2.0;
+	}
+	public String toString(){
+		return super.toString()+" (ID required)"; 
 	}
 }
